649acc26843623e7c8e953728139387cdab3f20c
Hua Do Gia Khanh <giakhanh2487@gmail.com>
2018-07-16 21:49:53 +0800

    Delete socialite.js

--- _assets/js/socialite.js
+++
@@ -1,8 +0,0 @@
-window.onload = function() {
-  var sl = document.createElement('LINK');
-  sl.attr('href', 'https://cdn.jsdelivr.net/npm/socialite.client@0.1.3/dist/css/main.css');
-  sl.attr('rel', 'stylesheet');
-  document.head.appendChild(sl);
-   
-  SOCIALITE('root');
-}
